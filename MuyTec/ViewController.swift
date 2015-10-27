5ffbf85379b9774e1a18b1d4a8253838bdd91b99
Efrain <jesusayllonroa@gmail.com>
2015-10-27 13:29:09 -0500

    Initial Commit

---
+++ MuyTec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuyTec
+//
+//  Created by Efrain Ayllon on 10/27/15.
+//  Copyright © 2015 Efrain Ayllon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
